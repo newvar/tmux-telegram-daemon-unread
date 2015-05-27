51350a09748b33d69725f803d9b1d81e80862dae
newvar <newvar@gmail.com>
2015-05-27 16:17:58 +0300

    status message for unexisting tg daemon -> as option

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -6,3 +6,7 @@ telegram_daemon_default_port="2391"
 telegram_daemon_timeout="@telegram-daemon-timeout"
 # 1 by default
 telegram_daemon_default_timeout="1"
+#Status message for unexisting tg daemon
+telegram_daemon_no_daemon_message="@telegram-daemon-no-daemon-message"
+# Default status message for unexisting tg daemon
+telegram_daemon_default_no_daemon_message="No Telegram at @port"
